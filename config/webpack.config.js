7cd1744d2e4deb08ecf7f941da8d76ab654ac2c5
Stuart Richardson <stu.richo@gmail.com>
2015-06-11 18:51:31 +1000

    initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,59 @@
+var fs = require('fs');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: './build',
+    publicPath: 'http://localhost:8080/',
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 versions'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!autoprefixer?browsers=last 2 versions!sass'
+      },
+      {
+        test: /\.png$/,
+        loader: "url?limit=100000&mimetype=image/png"
+      },
+      {
+        test: /\.svg$/,
+        loader: "url?limit=100000&mimetype=image/svg+xml"
+      },
+      {
+        test: /\.gif$/,
+        loader: "url?limit=100000&mimetype=image/gif"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss', '.css'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './config/template.html',
+    })
+  ]
+};
