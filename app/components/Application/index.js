c613f1582b89e738a4633afba570d52ecacc1b1e
Stuart Richardson <stu.richo@gmail.com>
2015-06-11 19:53:50 +1000

    this will do

--- app/components/Application/index.js
+++ app/components/Application/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { Dialog, FlatButton, Styles } from 'material-ui';
+
+const ThemeManager = Styles.ThemeManager();
+const Colors = Styles.Colors;
 
 require('./styles');
 
 class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: null
+    }
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
+  componentWillMount() {
+    ThemeManager.setPalette({
+      accent1Color: Colors.deepOrange500
+    });
+  }
+
   render() {
     return (
       <div className='Application'>
         <h1 className='Application-title'>Timesheet 2 Application!</h1>
+        <FlatButton primary={true}>Hi There</FlatButton>
       </div>
     )
   }
 }
+
+Application.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
 
 export default Application;
